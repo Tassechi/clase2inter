a38284277d47fbd43406af378cde02254982989a
Tassechi <35828746+Tassechi@users.noreply.github.com>
2018-04-18 21:16:37 -0600

    clase 2
    contenido

---
+++ app/src/main/java/clase2inter/com/clase2inter/MainActivity.java
@@ -0,0 +1,38 @@
+package clase2inter.com.clase2inter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1,btn2,btn3;
+    EditText txt1, txt2, txt3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txt1 = (EditText) findViewById(R.id.txt1);
+        txt2 = (EditText) findViewById(R.id.txt2);
+        txt3 = (EditText) findViewById(R.id.txt3);
+
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,txt1.getText().toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
